458ac1bf8ad05f40e77fc018b0e071e4bf66de0e
taip6494 <palisseauthomas@gmail.com>
2022-01-18 09:39:01 +0400

    6 - Bonne ou mauvaise réponse

--- lib/main.dart
+++ lib/main.dart
@@ -21,83 +21,84 @@ class body extends State<MyApp> {
   final check = Icon(Icons.check, color: Colors.green);
   final close = Icon(Icons.close, color: Colors.red);
   List<Icon> iconsList = [];
-  var question = ["Le piton des neiges est un volcan de la Réunion ?","Flutter permet de faire des applications web également ?","Php est le language utilisé par Flutter ?"];
+  var question = [
+    "Le piton des neiges est un volcan de la Réunion ?",
+    "Flutter permet de faire des applications web également ?",
+    "Php est le language utilisé par Flutter ?"
+  ];
   int numberquestion = 0;
-  void ajoutItems(icones){
+  List<bool> reponses = [true, false, true];
+
+  void ajoutItems(icones) {
     setState(() {
       iconsList.add(icones);
     });
   }
 
   @override
-
   Widget build(BuildContext context) {
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            flex: 5,
-            child:
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Center(
-                child: Text(
-
-                  (question[numberquestion]),
-                  style: TextStyle(color: Colors.white, fontSize: 25),
-                ),
-              ),
+    return Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+      Expanded(
+        flex: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Center(
+            child: Text(
+              (question[numberquestion]),
+              style: TextStyle(color: Colors.white, fontSize: 25),
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: (
-                  ElevatedButton(
-                      child: Text(
-                        'Vrai',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.green, // background (button) color
-                        onPrimary: Colors.white,
-                      ),
-                      onPressed: () {
-                      if (numberquestion < question.length - 1) numberquestion++;
-                        ajoutItems(check);
-                      }
-
-                  )
+        ),
+      ),
+      Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: (ElevatedButton(
+              child: Text(
+                'Vrai',
+                style: TextStyle(fontSize: 20),
               ),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: (
-                  ElevatedButton(
-                    child: Text(
-                      'faux',
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red, // background (button) color
-                      onPrimary: Colors.white,
-                    ),
-                    onPressed: () {
-                      if (numberquestion < question.length - 1) numberquestion++;
-                      ajoutItems(close);
-                    },
-                  )
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // background (button) color
+                onPrimary: Colors.white,
               ),
-            ),
-          ),
-          Row(
-            children:
-              iconsList,
-          )
-        ]
-    );
+              onPressed: () {
+                bool bonnereponse = reponses[numberquestion];
+                if (bonnereponse == true) {
+                  ajoutItems(check);
+                } else {
+                  ajoutItems(close);
+                }
+                numberquestion++;
+              })),
+        ),
+      ),
+      Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: (ElevatedButton(
+              child: Text(
+                'faux',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red, // background (button) color
+                onPrimary: Colors.white,
+              ),
+              onPressed: () {
+                bool bonnereponse = reponses[numberquestion];
+                if (bonnereponse == true) {
+                  ajoutItems(close);
+                } else {
+                  ajoutItems(check);
+                }
+                numberquestion++;
+              })),
+        ),
+      ),
+      Row(
+        children: iconsList,
+      )
+    ]);
   }
-
 }
